a89ed8dc5a73c9dfccd4fd6ec7022683030208e4
Jack Hodgkinson <jdahodgkinson@gmail.com>
2021-06-22 12:08:32 +0100

    Updated prior to distro hop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
+import React from 'react';
 import './App.css';
+import './sidebar.css';
 
-function App() {
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
+
+
+
+export default function App() {
   return (
-      <div className='app'>
-          <h1>coming soon</h1>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/article">
+            <Article />
+          </Route>
+          <Route path="/">
+            <Greeting />
+          </Route>
+        </Switch>
       </div>
+    </Router>
   );
 }
 
-export default App;
+function Sidebar() {
+  return (
+    <nav className="sidebar">
+      <h1 className="brand"><a href="#">jhodg.dev</a></h1>
+      <a href="#">Development</a>
+      <a href="#">Football</a>
+      <a href="#">Video Games</a>
+    </nav>
+  );
+}
+
+function Article() {
+  return (
+    <div>
+      <Sidebar/>
+      <div className="content">
+        <p>Article coming here in just a second...</p>
+      </div>
+    </div>
+  );
+}
+
+function Greeting() {
+  return (
+    <div className='greeting'>
+      <h1>coming soon</h1>
+    </div>
+  );
+}
